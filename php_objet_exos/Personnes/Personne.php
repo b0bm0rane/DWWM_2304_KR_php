8da3ae2c81afeccf6ccaca1cb9317c876d19f65a
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-11-30 11:44:54 +0100

    exos php objet personne

--- php_objet_exos/Personnes/Personne.php
+++ php_objet_exos/Personnes/Personne.php
@@ -4,11 +4,37 @@ class Personne
 {
     public string $firstName;
     public string $lastName;
-    public DateTime $birthday;
+    public string $birthday;
 
-    public function __construct(string $firstName, string $lastName, DateTime $birthday) {
+    public function __construct(string $firstName, string $lastName, string $birthday) {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->birthday = $birthday;
     }
+
+    public function getFirstName() : string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName() : string
+    {
+        return $this->lastName;
+    }
+    
+    public function getBirthday() : string
+    {
+        return $this->birthday;
+    }
+
 }
+
+$kevin = new Personne("Kévin", "Roussotte", "1993-07-16");
+
+echo $kevin->getFirstName();
+echo "<br>";
+echo $kevin->getLastName();
+echo "<br>";
+echo $kevin->getBirthday();
+echo "<br>";
+var_dump($kevin);
