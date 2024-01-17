34b75321aed2af4c279b1e612fdb5c17d4cb4597
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2024-01-17 15:23:39 +0100

    php pokemon

--- POKEMON/pokemon.php
+++ POKEMON/pokemon.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Pokédex</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+
 </head>
 <body>
 
@@ -82,6 +84,8 @@
                 $data = [];
                 $nb = 0;
 
+                echo "<h3>Mes pokémon</h3>";
+                // echo "<caption> Mes pokémon : </caption><table class='table table-striped table-hover'>";
                 echo "  <table> 
                             <thead> 
                                 <tr> 
@@ -101,7 +105,7 @@
                                             foreach ($obj as $key => $value) {
                                                echo '<td>' .$obj->$key. '</td>';
                                             }
-                                    echo "</tr>";
+                                echo "</tr>";
                                 }
                 echo"       </tbody>
                         </table>";
@@ -143,5 +147,8 @@
     
 
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 </html>
