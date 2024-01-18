12a3555b10596ad0160ac93980219316a603d54a
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2024-01-18 14:51:27 +0100

    php pokemon

--- POKEMON/pokemon.php
+++ POKEMON/pokemon.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr-fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,46 +13,41 @@
 
 <body>
     <?php
-    
-        
-       
-        //  require_once "Connexion.php"
-       
-       require_once "Connexion.php";
-       if (isset($_POST) && !empty($_POST["numero"]))
-       {
 
-          $monPDO=Connexion::getinstance();
-      
-          $rq="INSERT INTO pokemon VALUES (:numero, :espece, :niveau, :type1, :type2)";
-          
-          $state=$monPDO->prepare($rq);
-          $state->bindParam(':numero', $_POST['numero'], PDO::PARAM_INT);
-          $state->bindParam(':espece', $_POST['espece'], PDO::PARAM_STR);
-          $state->bindParam(':niveau', $_POST['niveau'], PDO::PARAM_INT);
-          $state->bindParam(':type1', $_POST['type1'], PDO::PARAM_STR);
-          $state->bindParam(':type2', $_POST['type2'], PDO::PARAM_STR);
-          $state->execute();
-            if( $state->rowcount()>0)
-            {
-                
-                // unset($_POST);
-                // header("refresh");
-                header("Location: pokemon.php");                
-            
-            }
 
-       
-      } else {
-        echo "<p>1er affichage du formulaire</p>";   
-      }
-      
-     
 
-    
-    
-    
-    
+    //  require_once "Connexion.php"
+
+    require_once "Connexion.php";
+    if (isset($_POST) && !empty($_POST["numero"])) {
+
+        $monPDO = Connexion::getinstance();
+
+        $rq = "INSERT INTO pokemon VALUES (:numero, :espece, :niveau, :type1, :type2)";
+
+        $state = $monPDO->prepare($rq);
+        $state->bindParam(':numero', $_POST['numero'], PDO::PARAM_INT);
+        $state->bindParam(':espece', $_POST['espece'], PDO::PARAM_STR);
+        $state->bindParam(':niveau', $_POST['niveau'], PDO::PARAM_INT);
+        $state->bindParam(':type1', $_POST['type1'], PDO::PARAM_STR);
+        $state->bindParam(':type2', $_POST['type2'], PDO::PARAM_STR);
+        $state->execute();
+        if ($state->rowcount() > 0) {
+
+            // unset($_POST);
+            // header("refresh");
+            header("Location: pokemon.php");
+        }
+    } else {
+        echo "<p>1er affichage du formulaire</p>";
+    }
+
+
+
+
+
+
+
     ?>
 
     <div>
@@ -60,7 +56,7 @@
 
     <h1>Mon pokédex</h1>
 
-    <form action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" enctype="multipart/form-data">
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" enctype="multipart/form-data">
         <section>
             <h2>Ajout d'un pokémon</h2>
         </section>
@@ -69,68 +65,68 @@
             <label for="numero">Numéro national (1-1024) : </label>
             <input type="number" name="numero" id="numero" step="1" placeholder="1" min="1" max="1024" required="true">
         </section>
-        
+
         <section>
             <label for="espece">Espèce du pokémon : </label>
             <input type="text" name="espece" id="espece" placeholder="pikachu" required>
         </section>
-        
+
         <section>
             <label for="niveau">Niveau du pokémon : </label>
             <input type="number" name="niveau" id="niveau" step="1" placeholder="1" min="1" max="100" required>
         </section>
-        
+
         <section>
             <label for="type1">Type 1 du pokémon : </label>
             <input type="text" name="type1" id="type1" placeholder="eau" required>
         </section>
-        
+
         <section>
-        <label for="type2">Type 2 du pokémon : </label>
+            <label for="type2">Type 2 du pokémon : </label>
             <input type="text" name="type2" id="type2" placeholder="feu">
         </section>
 
         <section>
             <input type="submit" value="Ajouter à mon Pokédex" id="ajout" name="ajout">
         </section>
-        </form>
-        <section>
-            <h3>Mes pokémon</h3>
+    </form>
+    <section>
+        <h3>Mes pokémon</h3>
+
             <select name="liste" id="liste">
-                <option value="">numero - espece - niveau - type1 - type2</option>
-                <?php
-                  
-
-                    $monPDO=Connexion::getinstance();
-                    $rq="SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
-                    $state=$monPDO->prepare($rq);
-                    $state->execute();
-                    
-                    while ($ligne = $state->fetch(PDO::FETCH_ASSOC)){
-
-                        //  echo '<option value="' . $ligne['numero'] . '">' . $ligne['espece'] . $ligne['niveau'] . $ligne['type_1'] . $ligne['type_2'] . '</option>';
-                        
-                            echo '<option value="'  . '">' . $ligne['numero'] . ' - ' . $ligne['espece'] . ' - ' . $ligne['niveau'] . ' - ' . $ligne['type_1'] . ' - ' . $ligne['type_2'] . '</option>';
-                    
-                    }    
-                ?>
-            </select>
-        </section>
-
-        <section>
+            <option value="">numero - espece - niveau - type1 - type2</option>
             <?php
-               //require_once "Connexion.php";
 
-                $monPDO=Connexion::getinstance();
-                $rq="SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
-                $state=$monPDO->prepare($rq);
-                $state->execute();
-                $data = [];
-                $nb = 0;
 
-                echo "<h3>Mes pokémon</h3>";
-                // echo "<caption> Mes pokémon : </caption><table class='table table-striped table-hover'>";
-                echo "  <table> 
+            $monPDO = Connexion::getinstance();
+            $rq = "SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
+            $state = $monPDO->prepare($rq);
+            $state->execute();
+
+            while ($ligne = $state->fetch(PDO::FETCH_ASSOC)) {
+
+                //  echo '<option value="' . $ligne['numero'] . '">' . $ligne['espece'] . $ligne['niveau'] . $ligne['type_1'] . $ligne['type_2'] . '</option>';
+
+                echo '<option value="'  . '">' . $ligne['numero'] . ' - ' . $ligne['espece'] . ' - ' . $ligne['niveau'] . ' - ' . $ligne['type_1'] . ' - ' . $ligne['type_2'] . '</option>';
+            }
+            ?>
+       </select>
+    </section>
+
+    <section>
+        <?php
+        //require_once "Connexion.php";
+
+        $monPDO = Connexion::getinstance();
+        $rq = "SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
+        $state = $monPDO->prepare($rq);
+        $state->execute();
+        $data = [];
+        $nb = 0;
+
+        echo "<h3>Mes pokémon</h3>";
+        // echo "<caption> Mes pokémon : </caption><table class='table table-striped table-hover'>";
+        echo "  <table> 
                             <thead> 
                                 <tr> 
                                     <th>Numéro national</th>
@@ -142,50 +138,25 @@
                             </thead>
                             <tbody>";
 
-                                while ($obj = $state->fetch()) {
-                                echo "<tr>";
-                                            $nb++;
-                                            array_push($data, $obj);
-                                            foreach ($obj as $key => $value) {
-                                               echo '<td>' .$obj->$key. '</td>';
-                                            }
-                                echo "</tr>";
-                                }
-                echo"       </tbody>
+        while ($obj = $state->fetch()) {
+            echo "<tr>";
+            $nb++;
+            array_push($data, $obj);
+            foreach ($obj as $key => $value) {
+                echo '<td>' . $obj->$key . '</td>';
+            }
+            echo "</tr>";
+        }
+        echo "       </tbody>
                         </table>";
 
-           
-                // echo   "<table>
-                //             <thead>
-                //                 <tr>
-                //                     <th>Numéro national</th>
-                //                     <th>Espèce</th>
-                //                     <th>Niveau</th>
-                //                     <th>Type 1</th>
-                //                     <th>Type 2</th> 
-                //                 </tr>
-                //             </thead>
-                //                 <tbody>";
-                //                     while ($obj = $state->fetch()){
-                //                         echo "  <tr>";
-                //                                     $nb++;
-                //                                     array_push($data, $obj);
-                //                                     foreach ($obj as key => $value){
-                //                                         echo '<td>' . $obj->key . '</td>';
-                //                                     }
-                //                         echo "  /tr";
-                //                     }
-                // echo "          </tbody>
-                //         </table>";
 
-                // echo $nb;
-
-            ?>
-        </section>
+        ?>
+    </section>
 
 
 
-    
+
 
 </body>
 
