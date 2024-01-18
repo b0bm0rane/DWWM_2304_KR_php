b6091d94d7f56009eb0ba547893daffd56d18182
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2024-01-18 12:27:58 +0100

    php pokemon

--- POKEMON/pokemon.php
+++ POKEMON/pokemon.php
@@ -8,7 +8,51 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
 </head>
+
+
 <body>
+    <?php
+    
+        
+       
+        //  require_once "Connexion.php"
+       
+       require_once "Connexion.php";
+       if (isset($_POST) && !empty($_POST["numero"]))
+       {
+
+          $monPDO=Connexion::getinstance();
+      
+          $rq="INSERT INTO pokemon VALUES (:numero, :espece, :niveau, :type1, :type2)";
+          
+          $state=$monPDO->prepare($rq);
+          $state->bindParam(':numero', $_POST['numero'], PDO::PARAM_INT);
+          $state->bindParam(':espece', $_POST['espece'], PDO::PARAM_STR);
+          $state->bindParam(':niveau', $_POST['niveau'], PDO::PARAM_INT);
+          $state->bindParam(':type1', $_POST['type1'], PDO::PARAM_STR);
+          $state->bindParam(':type2', $_POST['type2'], PDO::PARAM_STR);
+          $state->execute();
+            if( $state->rowcount()>0)
+            {
+                
+                // unset($_POST);
+                // header("refresh");
+                header("Location: pokemon.php");                
+            
+            }
+
+       
+      } else {
+        echo "<p>1er affichage du formulaire</p>";   
+      }
+      
+     
+
+    
+    
+    
+    
+    ?>
 
     <div>
         <img src="./img/logo_pokemon.png" alt="Logo pokémon" id="logo">
@@ -16,29 +60,29 @@
 
     <h1>Mon pokédex</h1>
 
-    <form action="<?=$_SERVER["PHP_SELF"]?>" method="POST" enctype="multipart/form-data">
+    <form action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" enctype="multipart/form-data">
         <section>
             <h2>Ajout d'un pokémon</h2>
         </section>
 
         <section>
             <label for="numero">Numéro national (1-1024) : </label>
-            <input type="number" name="numero" id="numero" step="1" placeholder="1" min="1" max="1024">
+            <input type="number" name="numero" id="numero" step="1" placeholder="1" min="1" max="1024" required="true">
         </section>
         
         <section>
             <label for="espece">Espèce du pokémon : </label>
-            <input type="text" name="espece" id="espece" placeholder="pikachu">
+            <input type="text" name="espece" id="espece" placeholder="pikachu" required>
         </section>
         
         <section>
             <label for="niveau">Niveau du pokémon : </label>
-            <input type="number" name="niveau" id="niveau" step="1" placeholder="1" min="1" max="100">
+            <input type="number" name="niveau" id="niveau" step="1" placeholder="1" min="1" max="100" required>
         </section>
         
         <section>
             <label for="type1">Type 1 du pokémon : </label>
-            <input type="text" name="type1" id="type1" placeholder="eau">
+            <input type="text" name="type1" id="type1" placeholder="eau" required>
         </section>
         
         <section>
@@ -47,15 +91,15 @@
         </section>
 
         <section>
-            <input type="submit" value="Ajouter à mon Pokédex" id="ajout">
+            <input type="submit" value="Ajouter à mon Pokédex" id="ajout" name="ajout">
         </section>
-
+        </form>
         <section>
             <h3>Mes pokémon</h3>
             <select name="liste" id="liste">
                 <option value="">numero - espece - niveau - type1 - type2</option>
                 <?php
-                    require_once "Connexion.php";
+                  
 
                     $monPDO=Connexion::getinstance();
                     $rq="SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
@@ -75,7 +119,7 @@
 
         <section>
             <?php
-                require_once "Connexion.php";
+               //require_once "Connexion.php";
 
                 $monPDO=Connexion::getinstance();
                 $rq="SELECT numero, espece, niveau, type_1, type_2 FROM pokemon";
@@ -110,10 +154,7 @@
                 echo"       </tbody>
                         </table>";
 
-
-
-
-
+           
                 // echo   "<table>
                 //             <thead>
                 //                 <tr>
@@ -142,7 +183,7 @@
             ?>
         </section>
 
-    </form>
+
 
     
 
